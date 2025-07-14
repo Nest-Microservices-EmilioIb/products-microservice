773c3243d798d8f45d815ac4e8fb93694e9179e8
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2025-07-14 08:40:42 -0600

    feat: validation of products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -77,4 +77,24 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       },
     });
   }
+
+  async validateProducts(ids: number[]) {
+    const uniqueIds = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: uniqueIds,
+        },
+      },
+    });
+
+    if (uniqueIds.length !== products.length)
+      throw new RpcException({
+        message: `Some products were not found.`,
+        status: HttpStatus.BAD_REQUEST,
+      });
+
+    return products;
+  }
 }
